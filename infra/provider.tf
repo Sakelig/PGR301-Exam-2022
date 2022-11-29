2281d2ec98143b3ef32aa7430df90c50d1d9407d
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-11-29 13:07:09 +0100

    DEL5 OPPGAVE1: Test2

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,10 @@ terraform {
       version = "4.40.0"
     }
   }
+  backend "s3" {
+      bucket = "pgr301-exam-2022-terraform-state"
+      key    = "1048/cloudwatch.state"
+      region = "eu-north-1"
+    }
 
 }
