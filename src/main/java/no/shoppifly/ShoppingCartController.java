4040680b44b9292f9e96b3decc42f2fc937ca49f
EC2 Default User <ec2-user@ip-172-31-36-202.eu-west-1.compute.internal>
2022-11-30 00:31:32 +0000

    random change test

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 @RestController()
 public class ShoppingCartController implements ApplicationListener<ApplicationReadyEvent> {
 
+
     private Map<String, Float> cartSum = new HashMap();
 
     @Autowired
